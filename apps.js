205fe9fe7cd933fc830ea31278dea381a20987e8
HunainAliArhsad <155620477+HunainAliArhsad@users.noreply.github.com>
2024-01-22 04:49:46 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,34 @@
+// let age=prompt("enter your age");
+// let a=Number(age);
+// console.log(a);
+// var isstudent=prompt("Do you have student id");
+// var b = "yes";
+// if(isstudent = b && isstudent === b){
+//     console.log("you are a student")
+// }
+// else{
+//     console.log("you are not a student")
+// };
+
+// ------------------------------------------------------------------------------------------------------------
+
+// alert("Hello there!\nI am Hunain, and I welcome you to this webpage. Feel free to explore!");
+
+
+// -------------------------------------------------------------------------------------------------------------------
+// let length=prompt("enter length of rectangle");
+// let a=Number(length);
+// let width=prompt("enter width of rectangle");
+// let b=Number(width);
+// let area= a * b;
+// console.log("The area of rectangle is:" + area);
+
+
+
+// ----------------------------------------------------------------------------------------------------------------
+
+let userName=prompt("enter your name");
+alert("Hello \n" + userName + "\n I welcome you to this webpage. Feel free to explore!");
+
+
+
